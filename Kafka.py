fae8f0742181f73dbb94ded6f5da16c8e2af3237
Carlos-21 <carlos.llontop3@unmsm.edu.pe>
2019-12-06 13:28:19 -0500

    final

--- Kafka.py
+++ Kafka.py
@@ -7,12 +7,14 @@ Created on Wed Nov 27 14:26:03 2019
 
 from OrdenCompra import OrdenCompraModel
 
+from datetime import date
+
 from kafka import KafkaConsumer, KafkaProducer
 from json import dumps, loads
 
 consumer = KafkaConsumer(
     'factura',
-     bootstrap_servers=['192.168.4.25:9092'],
+     bootstrap_servers=['192.168.4.7:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='fact',
@@ -31,20 +33,22 @@ while(True):
         ordenCompra.sEstado = message["sEstado"]
         ordenCompra.lDetalleCompra = message["lDetalleCompra"]
         
-        
         json = {
                     "nCodigoOrden" : ordenCompra.nCodigoOrden,
                     "sRucCliente" : ordenCompra.sRucCliente,
                     "nPrecioTotal" : ordenCompra.nPrecioTotal,
                     "sEstado" : "Reservado",
+                    "fechaFactura" : date.today().strftime("%Y-%m-%d, %H:%M:%S"),
                     "lDetalleCompra" : ordenCompra.lDetalleCompra
                 }
+        
         print(dumps(json))
     
-        producer = KafkaProducer(bootstrap_servers=['192.168.4.25:9092'],
+        producer = KafkaProducer(bootstrap_servers=['192.168.4.7:9092'],
                              value_serializer=lambda x: 
                              dumps(x).encode('utf-8'))
-        producer.send('cuentas', value=json)    
+        producer.send('cuentas', value=json)   
+        producer.send('cuentas2', value=json)  
         
     
     
